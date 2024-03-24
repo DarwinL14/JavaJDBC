790c086d7d8207084cd3fd8108a5cdc7e3168a26
DarwinL14 <dslesmes6@soy.sena.edu.co>
2024-03-24 11:42:15 -0500

    primera version

---
+++ src/main/java/co/edu/sena/main.java
@@ -0,0 +1,13 @@
+package co.edu.sena;
+
+/**
+ * Hello world!
+ *
+ */
+public class main 
+{
+    public static void Main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
